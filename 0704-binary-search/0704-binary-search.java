49c707c2a2b4f87668593da1003eb7c36d9a7182
yaminidhalavayi <yaminidhalavayi@gmail.com>
2026-08-28 14:42:42 +0530

    Time: 0 ms (100%), Space: 48.6 MB (14.04%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,26 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left <= right) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
             
-            int mid = left + (right - left) / 2;
-            
+            int mid = low + (high - low) / 2;
+
             if (nums[mid] == target) {
                 return mid;
             }
             
             else if (nums[mid] < target) {
-                left = mid + 1;
+                low = mid + 1;
             }
             
             else {
-                right = mid - 1;
+                high = mid - 1;
             }
         }
-        
+
         return -1;
     }
 }
